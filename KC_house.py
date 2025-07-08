74f9ecc4d9ed91362e87d99c3f7877616c90b0a1
elifsenasoysal <168901373+elifsenasoysal@users.noreply.github.com>
2025-07-08 11:19:01 +0300

    Update KC_house.py

--- KC_house.py
+++ KC_house.py
@@ -5,6 +5,11 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
+
 
 df=pd.read_csv('kc_house_data.csv') 
 
@@ -61,3 +66,46 @@ df_clean['age']=2015-df_clean['yr_built'] #evin yaşını hesaplıyoruz en son 2
 df_clean['restore']=np.where(df_clean['yr_renovated']==0,0,1)# restorasyon yapılmış mı yapılmamış mı sıfırsa edilmemiş 1 se edilmiş
 df_clean['sqft_total']=df_clean['sqft_living']+df_clean['sqft_lot']
 df_clean['sqft_basement']=np.where(df_clean['sqft_basement']==0,0,1) # bodrum var mı yok mu
+print(df_clean.shape)
+X=df_clean.drop(['price','date','id','yr_built','yr_renovated','lat','long','sqft_living','sqft_lot'],axis=1)
+y=df_clean['price']
+print(X.shape)
+print(y.shape)
+print(X.info()) 
+#kategorilere cevirim
+X=pd.get_dummies(X,columns=['zipcode'],drop_first=True)
+#eksik veri yok sadece zip, kod one hot encosing ile katagori sayısal değere çevirildi
+print(X.info())
+#standart ölçeklemdirici nesnesini kullanarak xi dönüştürüyor 
+scaler=StandardScaler()
+X=scaler.fit_transform(X)
+# veri setini eğitim seti olarak böl
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
+#%80 train %20 test. validation yok 
+#modeli seç ve oluştur 
+model=LinearRegression()
+#modeli eğitimi
+model.fit(X_train,y_train)
+#modeli test et
+y_pred=model.predict(X_test)
+print("R2 score: ",r2_score(y_test,y_pred))
+print("Mean Squared Error: ",mean_squared_error(y_test,y_pred)**.5)
+#çıktı: R2 score:  0.8243859850894387, Mean Squared Error:  114297.33540555707
+
+#başka bir model denemek istiyoruz random forest
+#model2
+print('Model 2:')
+model=RandomForestRegressor()
+model.fit(X_train,y_train)
+y_pred=model.predict(X_test)
+print("R2 score: ",r2_score(y_test,y_pred))
+print("Mean Squared Error: ",mean_squared_error(y_test,y_pred)**.5)
+#çıktı: R2 score:  0.8083788937941847, Mean Squared Error:  119392.81484997652 daha kötü çıktı 
+
+#model 3
+print('Model 3:')
+model=XGBRegressor()
+model.fit(X_train,y_train)
+y_pred=model.predict(X_test)
+print("R2 score: ",r2_score(y_test,y_pred))
+print("Mean Squared Error: ",mean_squared_error(y_test,y_pred)**.5)
